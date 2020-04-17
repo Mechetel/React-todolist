c172651aec462d9651b2da24520ea96f68d7e004
mechetel <dima.homa5@gmail.com>
2020-04-17 20:19:57 +0300

    style: added svgs and some style refactor

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import './App.css';
+import List from './components/List'
 
 function App() {
-  return (
-  <h1>123</h1>
-  );
+  return <div className="todo">
+    <div className="todo__sidebar">
+      <List />
+    </div>
+    <div className="todo__tasks"></div>
+  </div>;
 }
 
 export default App;
